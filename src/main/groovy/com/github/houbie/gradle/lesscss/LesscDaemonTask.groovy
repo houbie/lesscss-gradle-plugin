f2e718b0ce579a8deac0a0cda0434c1d5f1f523b
houbie <ivo@houbrechts-it.be>
2014-03-27 16:58:28 +0100

    copyright

--- src/main/groovy/com/github/houbie/gradle/lesscss/LesscDaemonTask.groovy
+++ src/main/groovy/com/github/houbie/gradle/lesscss/LesscDaemonTask.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2014 Houbrechts IT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.houbie.gradle.lesscss
 
 import com.github.houbie.lesscss.builder.CompilationTask
@@ -19,9 +35,12 @@ class LesscDaemonTask extends DefaultTask {
                 CompilationTask compilationTask = lesscTask.createCompilationTask()
                 logger.info("starting lessc daemon...")
                 compilationTask.startDaemon(interval)
+
+                def msg = "Lessc daemon is running. Press enter to quit..."
                 if (System.console()) {
-                    System.console().readLine("Lessc daemon is running. Press enter to quit...")
+                    System.console().readLine(msg)
                 } else {
+                    println msg
                     System.in.read()
                 }
                 compilationTask.stopDaemon()
